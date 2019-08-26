61b7240e852eafb65aedc7d1880691946cab4263
Devodriq Roberts <Devorb26@gmail.com>
2019-08-25 23:21:26 -0400

    Added plotly charts to py file, added notebook export

--- flight_data_analysis.py
+++ flight_data_analysis.py
@@ -90,8 +90,24 @@ weekday_dict = {
     7 : 'Sunday',
 }
 
+month_dict = {
+    1 : 'Jan',
+    2 : 'Feb',
+    3 : 'Mar', 
+    4 : 'Apr',
+    5 : 'May',
+    6 : 'Jun', 
+    7 : 'Jul', 
+    8 : 'Aug',
+    9 : 'Sep',
+    10 : 'Oct',
+    11 : 'Nov',
+    12 : 'Dec'
+}
+
 flights_df['DAY_OF_WEEK'] = flights_df['DAY_OF_WEEK'].map(weekday_dict)
 flights_df['flight_date'] = [datetime(year, month, day) for year, month, day in zip(flights_df.YEAR, flights_df.MONTH, flights_df.DAY)]
+flights_df['MONTH'] = flights_df['MONTH'].map(month_dict)
 flights_df.head()
 #%%
 print(f'Dataframe has {flights_df.shape[0]} rows, and {flights_df.shape[1]} columns.')
@@ -168,17 +184,226 @@ origin_airport_group = combined_df.groupby('ORIGIN_AIRPORT')['FLIGHT_NUMBER'].co
 
 
 #%% [markdown]
+# ## Destination Airport Distribution
+#%%
+destination_airport_group = combined_df.groupby('DESTINATION_AIRPORT')['FLIGHT_NUMBER'].count().sort_values(ascending=False)
+
+
+#%% [markdown]
 # ## Airline Distribution
 #%%
 airline_group = combined_df.groupby('AIRLINE')['FLIGHT_NUMBER'].count().sort_values(ascending=False)
 
 
+#%% [markdown]
+# ### Top 10 Origin Airport Distribution # of flights
 #%%
-origin_airport_group[1:11]
+labels = list(origin_airport_group[1:11].index)
+values = list(origin_airport_group[1:11].values)
+
+trace = go.Pie(labels=labels, values=values)
+layout = go.Layout(title='Origin Airport Flight Distribution (Top 10)',
+                    autosize=False,
+                    width=800,
+                    height=500,
+                    margin=go.layout.Margin(
+                        l=50,
+                        r=50,
+                        b=100,
+                        t=100,
+                        pad=4
+                    ))
+
+fig = go.Figure(data=[trace], layout=layout)
+iplot(fig, filename='origin_distribution')
+
+
+#%% [markdown]
+# No suprise that flight distributions for both Origin and Destinaton airports are very similar.
+#
+
+#%% [markdown]
+# ### Top 10 Destination Airport Distribution # of flights
+#%%
+labels = list(destination_airport_group[1:11].index)
+values = list(destination_airport_group[1:11].values)
+
+trace = go.Pie(labels=labels, values=values)
+layout = go.Layout(title='Destination Airport Flight Distribution',
+                    autosize=False,
+                    width=800,
+                    height=500,
+                    margin=go.layout.Margin(
+                        l=50,
+                        r=50,
+                        b=100,
+                        t=100,
+                        pad=4
+                    ))
+
+fig = go.Figure(data=[trace], layout=layout)
+iplot(fig, filename='destination_distribution')
+
 
 #%%
-airline_group[:10]
+#%% [markdown]
+# ### Top 10 Airlines Distribution # of flights
+#%%
+labels = list(airline_group[:10].index)
+values = list(airline_group[:10].values)
 
+trace = go.Pie(labels=labels, values=values)
+layout = go.Layout(title='Flight Distribution by Airline',
+                    autosize=False,
+                    width=800,
+                    height=500,
+                    margin=go.layout.Margin(
+                        l=50,
+                        r=50,
+                        b=100,
+                        t=100,
+                        pad=4
+                    ))
+
+fig = go.Figure(data=[trace], layout=layout)
+iplot(fig, filename='airline_distribution')
+
+
+
+
+#%% [markdown]
+# ### Top 20 Origin City Distribution # of flights
+#%%
+city_group = combined_df.groupby(['ORIGIN_CITY'])['FLIGHT_NUMBER'].count().sort_values(ascending=False)
+city_group[1:21]
+
+trace = go.Bar(x=city_group[1:21].index, 
+                y=city_group[1:21].values, 
+                name='city',
+                marker={
+                    'color':city_group[1:21].values,
+                    'colorscale':'Reds',
+                    'showscale':True,
+                    },
+                )
+
+layout = go.Layout(title='Number of Flights from Origin City',
+                    xaxis={'title':'Origin City'},
+                    yaxis={'title':'# of Flights'},
+                    autosize=False,
+                    width=800,
+                    height=500,
+                    margin=go.layout.Margin(
+                        l=50,
+                        r=50,
+                        b=100,
+                        t=100,
+                        pad=4
+                    ))
+
+fig = go.Figure(data=[trace], layout=layout)
+iplot(fig, filename='origin_city_bar')
+
+
+#%% [markdown]
+# ### Top 10 Airlines Distribution # of flights
+#%%
+
+trace = go.Bar(x=airline_group[:10].index, 
+                y=airline_group[:10].values, 
+                name='airlines',
+                marker={
+                    'color':airline_group[:10].values,
+                    'colorscale':'Reds',
+                    'showscale':True,
+                    },
+                )
+
+layout = go.Layout(title='Number of Flights by Airline',
+                    xaxis={'title':'Airline'},
+                    yaxis={'title':'# of Flights'},
+                    autosize=False,
+                    width=800,
+                    height=500,
+                    margin=go.layout.Margin(
+                        l=50,
+                        r=50,
+                        b=100,
+                        t=100,
+                        pad=4
+                    ))
+
+fig = go.Figure(data=[trace], layout=layout)
+iplot(fig, filename='airline_bar')
+
+
+#%% [markdown]
+# ### Distribution # of flights per Month
+#%%
+month_group = combined_df.groupby(['MONTH'])['FLIGHT_NUMBER'].count()
+
+trace = go.Bar(x=month_group.index, 
+                y=month_group.values, 
+                name='month',
+                marker={
+                    'color':month_group.values,
+                    'colorscale':'Reds',
+                    'showscale':True,
+                    },
+                )
+
+layout = go.Layout(title='Number of Flights per Month',
+                    xaxis={'title':'Month'},
+                    yaxis={'title':'# of Flights'},
+                    autosize=False,
+                    width=500,
+                    height=500,
+                    margin=go.layout.Margin(
+                        l=50,
+                        r=50,
+                        b=100,
+                        t=100,
+                        pad=4
+                    )
+                )
+
+fig = go.Figure(data=[trace], layout=layout)
+iplot(fig, filename='month_bar')
+
+
+#%% [markdown]
+# ### Distribution # of flights per Day of the Week
+#%%
+day_group = combined_df.groupby(['DAY_OF_WEEK'])['FLIGHT_NUMBER'].count().sort_values(ascending=False)
+
+trace = go.Bar(x=day_group.index, 
+                y=day_group.values, 
+                name='day_of_week',
+                marker={
+                    'color':day_group.values,
+                    'colorscale':'Reds',
+                    'showscale':True,
+                    },
+                )
+
+layout = go.Layout(title='Number of Flights per Day Of Week',
+                    xaxis={'title':'Day Of Week'},
+                    yaxis={'title':'# of Flights'},
+                    autosize=False,
+                    width=500,
+                    height=500,
+                    margin=go.layout.Margin(
+                        l=50,
+                        r=50,
+                        b=100,
+                        t=100,
+                        pad=4
+                    )
+                )
+
+fig = go.Figure(data=[trace], layout=layout)
+iplot(fig, filename='day_bar')
 
 
 #%%
+# flights_df.head(30)
